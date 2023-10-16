e789005fb7522e085d2ba00218bf374f208fafa7
Roooooooooooookas <rokas.kartenis@gmail.com>
2023-10-17 02:13:59 +0300

    Content addition
    - Added a form;
    - Dachshund's informational page added;

--- src/main.js
+++ src/main.js
@@ -5,25 +5,23 @@ let mobileAlt = document.getElementById("mobile-alt");
 
 // Check window size
 function checkWindowSize() {
-    return window.innerWidth >= 950; // Adjust the size range as needed
+    return window.innerWidth >= 950;
 }
 
 function toggleMoreMenu(status) {
     if(checkWindowSize()) { 
         if(moreMenu.style.display === "block" || status == false) {
-            // Hide the menu with a shrink animation
             moreMenu.style.opacity = "0";
             moreIcon.style.transform = "rotate(180deg)";
             setTimeout(function() {
                 moreMenu.style.display = "none";
-            }, 100); // Adjust the duration to match your transition duration
+            }, 100);
         } else {
-            // Show the menu with an enlarge animation
                 moreIcon.style.transform = "rotate(0deg)";
                 moreMenu.style.display = "block";
             setTimeout(function() {
                 moreMenu.style.opacity = "100%";
-            }, 0); // A small delay to ensure smooth animation
+            }, 0);
         }
     } else {
         if(mobileAlt.style.display === "grid") {
@@ -42,19 +40,17 @@ let navButton = document.getElementById("nav-button");
 function toggleNav(status) {
     if(window.innerWidth <= 950) {
         if(mainMenu.style.display === "block") {
-            // Hide the menu with a shrink animation
             mainMenu.style.opacity = "0";
             setTimeout(function() {
                 navButton.innerHTML = `<i class="material-symbols-rounded"> menu </i>`
                 mainMenu.style.display = "none";
-            }, 100); // Adjust the duration to match your transition duration
+            }, 100);
         } else {
-            // Show the menu with an enlarge animation
             mainMenu.style.display = "block";
             setTimeout(function() {
                 navButton.innerHTML = `<i class="material-symbols-rounded"> close </i>`
                 mainMenu.style.opacity = "100%";
-            }, 100); // A small delay to ensure smooth animation
+            }, 100)
         }
     }
 }
@@ -62,15 +58,6 @@ function toggleNav(status) {
 const homeButton = document.getElementById("home-button");
 const navLinks = document.getElementById("nav-links");
 const menuIcon = document.getElementById("menu-icon");
-
-homeButton.addEventListener("click", function (e) {
-    e.preventDefault(); // Prevent the default behavior of the link
-
-    // Smoothly scroll to the top of the page
-    window.scrollTo({
-        top: 0
-    });
-});
 
 window.addEventListener("resize", () => {
     if(checkWindowSize()) {
@@ -95,12 +82,20 @@ window.addEventListener("resize", () => {
 // Add a scroll event listener
 let isActivated = false;
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 0 && !isActivated) {
-        // Activate when scrolled down
-        if(checkWindowSize()) { 
-            console.log("Activated");
-            // You can place your activation code here.
-            navLinks.style.opacity = "0";
+    if(window.scrollY > 0 && !isActivated) {
+        console.log("Activated");
+
+        homeButton.addEventListener("click", function (e) {
+            e.preventDefault(); // Prevent the default behavior of the link
+        
+            // Smoothly scroll to the top of the page
+            window.scrollTo({
+                top: 0
+            });
+        });
+
+        if(checkWindowSize()) {
+            navLinks.style.opacity = "100%";
             toggleMoreMenu(false);
 
         
@@ -109,22 +104,40 @@ window.addEventListener("scroll", () => {
             pet_supplies </i>`;
         
             isActivated = true;
-        } 
-    } if (window.scrollY === 0 && isActivated) {
-        // Deactivate when scrolled back to the top
+        }
+    } if(window.scrollY === 0 && isActivated) {
         console.log("Deactivated");
-        // You can place your deactivation code here.
+
         navLinks.style.opacity = "100%";
     
         homeButton.classList.remove("home-button-add");
+        homeButton.innerHTML = `<i class="material-symbols-rounded" id="menu-icon">
+        pet_supplies </i> Home`;
+
         menuIcon.style.position = "absolute";
         menuIcon.style.top = "10px";
         menuIcon.style.left = "20px";
     
-        homeButton.innerHTML = `<i class="material-symbols-rounded" id="menu-icon">
-        pet_supplies </i> Home`;
-    
     
         isActivated = false;
+    } if(window.scrollY === 0) {
+        homeButton.addEventListener("click", function (e) {
+            e.preventDefault(); // Prevent the default behavior of the link
+        
+            window.location.href = 'index.html';
+        });
     }
+});
+
+const mainImage = document.getElementById('main-img');
+const thumbnails = document.querySelectorAll('.thumbnail');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        mainImage.style.opacity = 0;
+        setTimeout(() => {
+            mainImage.src = thumbnail.src;
+            mainImage.style.opacity = 1;
+        }, 100);
+    });
 });
